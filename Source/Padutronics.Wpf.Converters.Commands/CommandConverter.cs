0098c8e3a1d6b215a1b720e70a853349eedf7ed0
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-27 13:06:38 +0200

    Add command converter

---
+++ Source/Padutronics.Wpf.Converters.Commands/CommandConverter.cs
@@ -0,0 +1,21 @@
+﻿using Padutronics.Commands;
+using Padutronics.Commands.Adapters.System.Windows.Input;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Padutronics.Wpf.Converters.Commands;
+
+[ValueConversion(sourceType: typeof(ICommand), targetType: typeof(System.Windows.Input.ICommand))]
+public sealed class CommandConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return new CommandToCommandAdapter((ICommand)value);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+}
